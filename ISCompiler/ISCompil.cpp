19c3f12f748eaa208c7562574c01f4a68610f28c
Lumito <50250351+LumitoLuma@users.noreply.github.com>
2020-08-24 22:30:05 +0200

    Removed *.js from .gitattributes and fixed errors in ISCompil.cpp

--- ISCompiler/ISCompil.cpp
+++ ISCompiler/ISCompil.cpp
@@ -3,19 +3,9 @@
 
 using namespace std;
 
-string exec;
-
 int main(int argc, char* argv[])
 {
-    if (argc > 1 && argv[1] = "hola")
-    {
-        printf("Compiling as release mode (EXPERIMENTAL!)\n");
-        exec = "\"" + (string)ProgFiles + (string)"\\Inno Setup 6\\iscc.exe\" ..\\Release.iss";
-    }
-    else
-    {
-        exec = "\"" + (string)ProgFiles + (string)"\\Inno Setup 6\\iscc.exe\" ..\\main.iss";
-    }
+    string exec = "\"" + (string)ProgFiles + (string)"\\Inno Setup 6\\iscc.exe\" ..\\main.iss";
     int err = system(exec.c_str());
     printf("\nThe program exited with code %d.\n", err);
     exit(err);
